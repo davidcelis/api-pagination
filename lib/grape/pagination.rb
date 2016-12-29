bcb428f41f6b95b4cc7c03e4e3ab01b2774786fe
David Celis <me@davidcel.is>
2016-12-28 22:54:18 -0500

    Coerce Grape's Total header to a String
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/grape/pagination.rb
+++ lib/grape/pagination.rb
@@ -27,7 +27,7 @@ module Grape
           include_total   = ApiPagination.config.include_total
 
           header 'Link',          links.join(', ') unless links.empty?
-          header total_header,    ApiPagination.total_from(collection) if include_total
+          header total_header,    ApiPagination.total_from(collection).to_s if include_total
           header per_page_header, options[:per_page].to_s
           header page_header,     options[:page].to_s unless page_header.nil?
 
