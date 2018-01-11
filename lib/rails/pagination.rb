58626b18534be28dc73dd8cce12901f31d6e854d
Thomas Schmidt <tschmidt@suse.de>
2018-01-11 18:37:09 +0100

    use request.path_info

--- lib/rails/pagination.rb
+++ lib/rails/pagination.rb
@@ -30,7 +30,7 @@ module Rails
       collection = ApiPagination.paginate(collection, options)
 
       links = (headers['Link'] || '').split(',').map(&:strip)
-      url   = base_url + request.original_fullpath.sub(/\?.*$/, '')
+      url   = base_url + request.path_info
       pages = ApiPagination.pages_from(collection)
 
       pages.each do |k, v|
