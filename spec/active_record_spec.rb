e3f490f9e8fcd79f0f88c7f73e5cc843439aab38
David Stancu <dstancu@nyu.edu>
2018-05-17 10:10:28 -0400

    clean up test

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -7,14 +7,35 @@ ActiveRecord::Base.establish_connection(
   schema: 'support/active_record/schema'
 )
 
-describe 'using kaminari with active_record' do
+shared_examples 'produces_correct_sql' do 
   let(:collection) { Foo.all }
   let(:per_page) { 5 }
 
   it 'produces correct sql for first page' do
     paginated_sql = ApiPagination.paginate(collection, per_page: per_page)
                                  .to_sql
-
     expect(paginated_sql).to eql(Foo.limit(per_page).offset(0).to_sql)
+  end
+end
+
+if ApiPagination.config.paginator == :kaminari
+  describe 'pagination with kaminari' do
+    before do 
+      ApiPagination.config.paginator = :kaminari
+    end
+
+    include_examples 'produces_correct_sql'
+  end
+end
+
+if ApiPagination.config.paginator == :will_paginate
+  require 'will_paginate/active_record'
+
+  describe 'pagination with will_paginate' do 
+    before do 
+      ApiPagination.config.paginator = :will_paginate
+    end
+
+    include_examples 'produces_correct_sql'
   end
 end
