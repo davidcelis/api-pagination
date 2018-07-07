3b6ae634bfd9c432f3e79372a30aaf65982fb8ea
David Stancu <dstancu@nyu.edu>
2018-07-07 11:20:49 -0400

    make spec match new api, reflections spec does not apply to pagy

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -11,14 +11,15 @@ NullDB.configure { |ndb| def ndb.project_root; Dir.pwd; end; }
 
 shared_examples 'produces_correct_sql' do 
   it 'produces correct sql for first page' do
-    paginated_sql = ApiPagination.paginate(collection, per_page: per_page)
-    .to_sql
-    expect(paginated_sql).to eql(Foo.limit(per_page).offset(0).to_sql)
+    allow(collection).to receive(:count).and_return(collection_size)
+    paginated_sql, _ = ApiPagination.paginate(collection, per_page: per_page)
+    expect(paginated_sql.to_sql).to eql(Foo.limit(per_page).offset(0).to_sql)
   end
 end
 
 describe 'ActiveRecord Support' do 
   let(:collection) { Foo.all }
+  let(:collection_size) { 50 }
   let(:per_page) { 5 }
   
   if ApiPagination.config.paginator == :will_paginate
@@ -29,16 +30,19 @@ describe 'ActiveRecord Support' do
     include_examples 'produces_correct_sql'
   end
 
-  context 'reflections' do
-    it 'invokes the correct methods to determine type' do 
-      expect(collection).to receive(:klass).at_least(:once)
-                                           .and_call_original
-      ApiPagination.paginate(collection)
-    end
 
-    it 'does not fail if table name is not snake cased class name' do
-      allow(collection).to receive(:table_name).and_return(SecureRandom.uuid)
-      expect { ApiPagination.paginate(collection) }.to_not raise_error
+  if ApiPagination.config.paginator != :pagy
+    context 'reflections' do
+      it 'invokes the correct methods to determine type' do 
+        expect(collection).to receive(:klass).at_least(:once)
+                                             .and_call_original
+        ApiPagination.paginate(collection)
+      end
+
+      it 'does not fail if table name is not snake cased class name' do
+        allow(collection).to receive(:table_name).and_return(SecureRandom.uuid)
+        expect { ApiPagination.paginate(collection) }.to_not raise_error
+      end
     end
   end
 end
